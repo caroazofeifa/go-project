22b4a10cfef038c1c64b35b72ded8a185a055ab1
Carolina Azofeifa <mcazofeifa@gmail.com>
2017-06-06 23:11:14 -0600

    Reads from db the contacts

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -15,7 +15,63 @@ type PageVariables struct {
         PageContacts []Person
 }
 func main() {
-         //MONGO
+        //SERVER
+	    http.HandleFunc("/", getContacts)
+            //http.HandleFunc("/add", postContacts)
+	    log.Fatal(http.ListenAndServe(":8082", nil))       
+}
+func getContacts(w http.ResponseWriter, r *http.Request){
+        //MONGO
+        session, err := mgo.Dial("localhost:27017/contacts")
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+
+        session.SetMode(mgo.Monotonic, true)
+
+        c := session.DB("contacts").C("people")
+
+        var m []bson.M
+        var x = c.Find(nil).All(&m)
+        fmt.Println(x)
+        slicePerson := make([]Person, 0)
+        for i,v := range m {
+                var name interface{}  = v["name"]
+                var phone interface{} = v["phone"]
+                slicePerson = append(slicePerson, Person{name.(string), phone.(string)})
+                fmt.Println(i)
+        }
+
+        MyContacts := slicePerson
+
+        fmt.Println("MyContacts;",slicePerson)
+        MyPageVariables := PageVariables{ 
+                PageContacts : MyContacts,
+        }
+
+        t, err := template.ParseFiles("index.html") 
+        if err != nil { 
+                log.Print("index parsing error: ", err) 
+                }
+        err = t.Execute(w, MyPageVariables) 
+        if err != nil { 
+                log.Print("index executing error: ", err) 
+  	}
+}
+func postContacts(w http.ResponseWriter, r *http.Request){
+        //MONGO
+        fmt.Println("Insert data from request")
+
+        // r.ParseForm()
+        // // r.Form is now either
+        // // map[animalselect:[cats]] OR
+        // // map[animalselect:[dogs]]
+        // // so get the animal which has been selected
+        // youranimal := r.Form.Get("animalselect")
+
+        // getContacts();
+
         /*session, err := mgo.Dial("localhost:27017/contacts")
         if err != nil {
                 panic(err)
@@ -30,55 +86,5 @@ func main() {
 	               &Person{"Cla", "+55 53 8402 8510"})
         if err != nil {
                 log.Fatal(err)
-        }
-        */
-        //SERVER
-	    http.HandleFunc("/", index)
-	    log.Fatal(http.ListenAndServe(":8082", nil))       
-}
-func index(w http.ResponseWriter, r *http.Request){
-        //MONGO
-        fmt.Println("Empieza el index")
-        session, err := mgo.Dial("localhost:27017/contacts")
-        if err != nil {
-                panic(err)
-        }
-        defer session.Close()
-
-        session.SetMode(mgo.Monotonic, true)
-
-        c := session.DB("contacts").C("people")
-
-        // result := Person{}
-        // err = c.Find(nil)
-        // if err != nil {
-        //         log.Fatal(err)
-        // }
-
-        var m []bson.M
-        var x = c.Find(nil).All(&m)
-        fmt.Println(x)
-        
-        MyContacts:=[]Person{
-            for i, v := range m {
-                Person{v["name"], "1234"}
-                //fmt.Println(v["name"])
-                fmt.Println(i)
-            }
-                //Person{"Alicia", "1234"},
-                //Person{"Aleman", "5678"},   
-        }
-        MyPageVariables := PageVariables{ //store the date and time in a struct
-                PageContacts : MyContacts,
-        }
-
-        t, err := template.ParseFiles("index.html") //parse the html file homepage.html
-        if err != nil { // if there is an error
-                log.Print("index parsing error: ", err) // log it
-                }
-        err = t.Execute(w, MyPageVariables) //execute the index and pass it the HomePageVars struct to fill in the gaps
-        //err = t.Execute(w, MyContacts) //execute the index and pass it the HomePageVars struct to fill in the gaps
-        if err != nil { // if there is an error
-                log.Print("index executing error: ", err) //log it
-  	}
+        }*/
 }
