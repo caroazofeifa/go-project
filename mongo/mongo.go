025d0fa1c2a5c3842734e9d8f5ccd7dfb14ac3a4
Carolina Azofeifa <mcazofeifa@gmail.com>
2017-06-12 14:01:59 -0600

    Trying put and delete

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -16,16 +16,87 @@ type PageVariables struct {
 }
 func main() {
         //SERVER
-	    http.HandleFunc("/", getContacts)
-            http.HandleFunc("/add", postContacts)
-	    log.Fatal(http.ListenAndServe(":8082", nil))       
+	http.HandleFunc("/", request)
+        //     http.HandleFunc("/add", postContacts)
+        //     http.HandleFunc("/delete", deleteContact)
+        fmt.Println("Running on port 8082")
+	log.Fatal(http.ListenAndServe(":8082", nil))       
 }
-
-func postContacts(w http.ResponseWriter, r *http.Request){
-        //MONGO
-        fmt.Println("Insert data from request")
+func request(w http.ResponseWriter, r *http.Request) {
         r.ParseForm()
         //fmt.Println(r)
+        switch r.Method {
+        case "GET":
+                getContacts(w,r)
+        // Serve the resource.
+        case "POST":
+                postContacts(w,r)
+        // Create a new record.
+        case "PUT":
+                updateContact(w,r)
+        // Update an existing record.
+        case "DELETE":
+                deleteContact(w,r)
+        // Remove the record.
+        default:
+                fmt.Println("Error")
+        }
+}
+func updateContact(w http.ResponseWriter, r *http.Request){
+        //MONGO
+        fmt.Println("PUT-Update")
+        r.ParseForm()
+        //fmt.Println(r)
+        // var name = r.Form.Get("name")
+        // var phone = r.Form.Get("number")
+        // // fmt.Println(name)
+        // // fmt.Println(phone)
+
+        // session, err := mgo.Dial("localhost:27017/contacts")
+        // if err != nil {
+        //         panic(err)
+        // }
+        // defer session.Close()
+        // // Optional. Switch the session to a monotonic behavior.
+        // session.SetMode(mgo.Monotonic, true)
+        // c := session.DB("contacts").C("people")
+        // err = c.Insert(&Person{name, phone})
+        // if err != nil {
+        //         log.Fatal(err)
+        // }
+        // getContacts(w,r)
+}
+func deleteContact(w http.ResponseWriter, r *http.Request){
+        //MONGO
+        fmt.Println("DELETE-Delete")
+        r.ParseForm()
+        fmt.Println(r)
+        fmt.Println(r.Form)
+        //params := r.URL.Query()
+        //params.Get("AppName") // returns 'Proline'
+        //sfmt.Println("caca: ",r.Delete(values))
+        //var name = r.Form.Get("data")
+        // fmt.Println(name)
+
+        // session, err := mgo.Dial("localhost:27017/contacts")
+        // if err != nil {
+        //         panic(err)
+        // }
+        // defer session.Close()
+        // // Optional. Switch the session to a monotonic behavior.
+        // session.SetMode(mgo.Monotonic, true)
+        // c := session.DB("contacts").C("people")
+        // err = c.Insert(&Person{name, phone})
+        // if err != nil {
+        //         log.Fatal(err)
+        // }
+        // getContacts(w,r)
+}
+func postContacts(w http.ResponseWriter, r *http.Request){
+        //MONGO
+        fmt.Println("POST-INSERT")
+        r.ParseForm()
+        fmt.Println(r)
         var name = r.Form.Get("name")
         var phone = r.Form.Get("number")
         // fmt.Println(name)
@@ -47,6 +118,7 @@ func postContacts(w http.ResponseWriter, r *http.Request){
 }
 func getContacts(w http.ResponseWriter, r *http.Request){
         //MONGO
+        fmt.Println("GET-SELECT")
         session, err := mgo.Dial("localhost:27017/contacts")
         if err != nil {
                 panic(err)
