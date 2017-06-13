9c222f0cc9501af8f668ec7a869e6aa314a497e5
Carolina Azofeifa <mcazofeifa@gmail.com>
2017-06-13 00:05:20 -0600

    css style working

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -5,9 +5,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"encoding/json"
 )
 
 type Person struct {
@@ -20,12 +20,13 @@ type PageVariables struct {
 
 func main() {
 	//SERVER
+	fs := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", request)
 	//     http.HandleFunc("/add", postContacts)
 	//     http.HandleFunc("/delete", deleteContact)
 	fmt.Println("Running on port 8082")
 	log.Fatal(http.ListenAndServe(":8082", nil))
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("/css/styles.css"))))
 }
 func request(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -46,56 +47,6 @@ func request(w http.ResponseWriter, r *http.Request) {
 	default:
 		fmt.Println("Error")
 	}
-}
-func updateContact(w http.ResponseWriter, r *http.Request) {
-	//MONGO
-	fmt.Println("PUT-Update")
-	r.ParseForm()
-	//fmt.Println(r)
-	// var name = r.Form.Get("name")
-	// var phone = r.Form.Get("number")
-	// // fmt.Println(name)
-	// // fmt.Println(phone)
-
-	// session, err := mgo.Dial("localhost:27017/contacts")
-	// if err != nil {
-	//         panic(err)
-	// }
-	// defer session.Close()
-	// // Optional. Switch the session to a monotonic behavior.
-	// session.SetMode(mgo.Monotonic, true)
-	// c := session.DB("contacts").C("people")
-	// err = c.Insert(&Person{name, phone})
-	// if err != nil {
-	//         log.Fatal(err)
-	// }
-	// getContacts(w,r)
-}
-func deleteContact(w http.ResponseWriter, r *http.Request) {
-	//MONGO
-	fmt.Println("DELETE-Delete")
-	r.ParseForm()
-	fmt.Println(r)
-	fmt.Println(r.Form)
-	//params := r.URL.Query()
-	//params.Get("AppName") // returns 'Proline'
-	//sfmt.Println("caca: ",r.Delete(values))
-	//var name = r.Form.Get("data")
-	// fmt.Println(name)
-
-	// session, err := mgo.Dial("localhost:27017/contacts")
-	// if err != nil {
-	//         panic(err)
-	// }
-	// defer session.Close()
-	// // Optional. Switch the session to a monotonic behavior.
-	// session.SetMode(mgo.Monotonic, true)
-	// c := session.DB("contacts").C("people")
-	// err = c.Insert(&Person{name, phone})
-	// if err != nil {
-	//         log.Fatal(err)
-	// }
-	// getContacts(w,r)
 }
 func postContacts(w http.ResponseWriter, r *http.Request) {
 	//MONGO
@@ -159,4 +110,30 @@ func getContacts(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Print("index executing error: ", err)
 	}
+}
+func updateContact(w http.ResponseWriter, r *http.Request) {
+	//MONGO
+	fmt.Println("PUT-Update")
+	fmt.Println(r)
+	todo := &Person{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(todo)
+	if err != nil {
+		fmt.Println("ERROR decoding JSON - ", err)
+		return
+	}
+	fmt.Println(todo)
+}
+func deleteContact(w http.ResponseWriter, r *http.Request) {
+	//MONGO
+	fmt.Println("DELETE-Delete")
+	fmt.Println(r)
+	todo := &Person{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(todo)
+	if err != nil {
+		fmt.Println("ERROR decoding JSON - ", err)
+		return
+	}
+	fmt.Println(todo)
 }
