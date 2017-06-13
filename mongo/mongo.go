fe7ce8a6e3454e18acc39ddce23c7c089bc7959f
Isaias Salas <isaias7salas@gmail.com>
2017-06-12 20:30:36 -0600

    add style

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -1,157 +1,162 @@
 package main
+
 import (
-        "html/template"
-        "fmt"
+	"fmt"
+	"html/template"
 	"log"
-        "gopkg.in/mgo.v2"
-        "gopkg.in/mgo.v2/bson"
-        "net/http"
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
 type Person struct {
-        Name string
-        Phone string
+	Name  string
+	Phone string
 }
 type PageVariables struct {
-        PageContacts []Person
+	PageContacts []Person
 }
+
 func main() {
-        //SERVER
+	//SERVER
 	http.HandleFunc("/", request)
-        //     http.HandleFunc("/add", postContacts)
-        //     http.HandleFunc("/delete", deleteContact)
-        fmt.Println("Running on port 8082")
-	log.Fatal(http.ListenAndServe(":8082", nil))       
+	//     http.HandleFunc("/add", postContacts)
+	//     http.HandleFunc("/delete", deleteContact)
+	fmt.Println("Running on port 8082")
+	log.Fatal(http.ListenAndServe(":8082", nil))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("/css/styles.css"))))
 }
 func request(w http.ResponseWriter, r *http.Request) {
-        r.ParseForm()
-        //fmt.Println(r)
-        switch r.Method {
-        case "GET":
-                getContacts(w,r)
-        // Serve the resource.
-        case "POST":
-                postContacts(w,r)
-        // Create a new record.
-        case "PUT":
-                updateContact(w,r)
-        // Update an existing record.
-        case "DELETE":
-                deleteContact(w,r)
-        // Remove the record.
-        default:
-                fmt.Println("Error")
-        }
+	r.ParseForm()
+	//fmt.Println(r)
+	switch r.Method {
+	case "GET":
+		getContacts(w, r)
+	// Serve the resource.
+	case "POST":
+		postContacts(w, r)
+	// Create a new record.
+	case "PUT":
+		updateContact(w, r)
+	// Update an existing record.
+	case "DELETE":
+		deleteContact(w, r)
+	// Remove the record.
+	default:
+		fmt.Println("Error")
+	}
 }
-func updateContact(w http.ResponseWriter, r *http.Request){
-        //MONGO
-        fmt.Println("PUT-Update")
-        r.ParseForm()
-        //fmt.Println(r)
-        // var name = r.Form.Get("name")
-        // var phone = r.Form.Get("number")
-        // // fmt.Println(name)
-        // // fmt.Println(phone)
+func updateContact(w http.ResponseWriter, r *http.Request) {
+	//MONGO
+	fmt.Println("PUT-Update")
+	r.ParseForm()
+	//fmt.Println(r)
+	// var name = r.Form.Get("name")
+	// var phone = r.Form.Get("number")
+	// // fmt.Println(name)
+	// // fmt.Println(phone)
 
-        // session, err := mgo.Dial("localhost:27017/contacts")
-        // if err != nil {
-        //         panic(err)
-        // }
-        // defer session.Close()
-        // // Optional. Switch the session to a monotonic behavior.
-        // session.SetMode(mgo.Monotonic, true)
-        // c := session.DB("contacts").C("people")
-        // err = c.Insert(&Person{name, phone})
-        // if err != nil {
-        //         log.Fatal(err)
-        // }
-        // getContacts(w,r)
+	// session, err := mgo.Dial("localhost:27017/contacts")
+	// if err != nil {
+	//         panic(err)
+	// }
+	// defer session.Close()
+	// // Optional. Switch the session to a monotonic behavior.
+	// session.SetMode(mgo.Monotonic, true)
+	// c := session.DB("contacts").C("people")
+	// err = c.Insert(&Person{name, phone})
+	// if err != nil {
+	//         log.Fatal(err)
+	// }
+	// getContacts(w,r)
 }
-func deleteContact(w http.ResponseWriter, r *http.Request){
-        //MONGO
-        fmt.Println("DELETE-Delete")
-        r.ParseForm()
-        fmt.Println(r)
-        fmt.Println(r.Form)
-        //params := r.URL.Query()
-        //params.Get("AppName") // returns 'Proline'
-        //sfmt.Println("caca: ",r.Delete(values))
-        //var name = r.Form.Get("data")
-        // fmt.Println(name)
+func deleteContact(w http.ResponseWriter, r *http.Request) {
+	//MONGO
+	fmt.Println("DELETE-Delete")
+	r.ParseForm()
+	fmt.Println(r)
+	fmt.Println(r.Form)
+	//params := r.URL.Query()
+	//params.Get("AppName") // returns 'Proline'
+	//sfmt.Println("caca: ",r.Delete(values))
+	//var name = r.Form.Get("data")
+	// fmt.Println(name)
 
-        // session, err := mgo.Dial("localhost:27017/contacts")
-        // if err != nil {
-        //         panic(err)
-        // }
-        // defer session.Close()
-        // // Optional. Switch the session to a monotonic behavior.
-        // session.SetMode(mgo.Monotonic, true)
-        // c := session.DB("contacts").C("people")
-        // err = c.Insert(&Person{name, phone})
-        // if err != nil {
-        //         log.Fatal(err)
-        // }
-        // getContacts(w,r)
+	// session, err := mgo.Dial("localhost:27017/contacts")
+	// if err != nil {
+	//         panic(err)
+	// }
+	// defer session.Close()
+	// // Optional. Switch the session to a monotonic behavior.
+	// session.SetMode(mgo.Monotonic, true)
+	// c := session.DB("contacts").C("people")
+	// err = c.Insert(&Person{name, phone})
+	// if err != nil {
+	//         log.Fatal(err)
+	// }
+	// getContacts(w,r)
 }
-func postContacts(w http.ResponseWriter, r *http.Request){
-        //MONGO
-        fmt.Println("POST-INSERT")
-        r.ParseForm()
-        fmt.Println(r)
-        var name = r.Form.Get("name")
-        var phone = r.Form.Get("number")
-        // fmt.Println(name)
-        // fmt.Println(phone)
+func postContacts(w http.ResponseWriter, r *http.Request) {
+	//MONGO
+	fmt.Println("POST-INSERT")
+	r.ParseForm()
+	fmt.Println(r)
+	var name = r.Form.Get("name")
+	var phone = r.Form.Get("number")
+	// fmt.Println(name)
+	// fmt.Println(phone)
 
-        session, err := mgo.Dial("localhost:27017/contacts")
-        if err != nil {
-                panic(err)
-        }
-        defer session.Close()
-        // Optional. Switch the session to a monotonic behavior.
-        session.SetMode(mgo.Monotonic, true)
-        c := session.DB("contacts").C("people")
-        err = c.Insert(&Person{name, phone})
-        if err != nil {
-                log.Fatal(err)
-        }
-        getContacts(w,r)
+	session, err := mgo.Dial("localhost:27017/contacts")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("contacts").C("people")
+	err = c.Insert(&Person{name, phone})
+	if err != nil {
+		log.Fatal(err)
+	}
+	getContacts(w, r)
 }
-func getContacts(w http.ResponseWriter, r *http.Request){
-        //MONGO
-        fmt.Println("GET-SELECT")
-        session, err := mgo.Dial("localhost:27017/contacts")
-        if err != nil {
-                panic(err)
-        }
-        defer session.Close()
+func getContacts(w http.ResponseWriter, r *http.Request) {
+	//MONGO
+	fmt.Println("GET-SELECT")
+	session, err := mgo.Dial("localhost:27017/contacts")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 
-        session.SetMode(mgo.Monotonic, true)
+	session.SetMode(mgo.Monotonic, true)
 
-        c := session.DB("contacts").C("people")
+	c := session.DB("contacts").C("people")
 
-        var m []bson.M
-        var _ = c.Find(nil).All(&m)
-        // fmt.Println(x)
-        slicePerson := make([]Person, 0)
-        for _,v := range m {
-                var name interface{}  = v["name"]
-                var phone interface{} = v["phone"]
-                slicePerson = append(slicePerson, Person{name.(string), phone.(string)})
-        }
+	var m []bson.M
+	var _ = c.Find(nil).All(&m)
+	// fmt.Println(x)
+	slicePerson := make([]Person, 0)
+	for _, v := range m {
+		var name interface{} = v["name"]
+		var phone interface{} = v["phone"]
+		slicePerson = append(slicePerson, Person{name.(string), phone.(string)})
+	}
 
-        MyContacts := slicePerson
+	MyContacts := slicePerson
 
-        // fmt.Println("MyContacts;",slicePerson)
-        MyPageVariables := PageVariables{ 
-                PageContacts : MyContacts,
-        }
+	// fmt.Println("MyContacts;",slicePerson)
+	MyPageVariables := PageVariables{
+		PageContacts: MyContacts,
+	}
 
-        t, err := template.ParseFiles("index.html") 
-        if err != nil { 
-                log.Print("index parsing error: ", err) 
-                }
-        err = t.Execute(w, MyPageVariables) 
-        if err != nil { 
-                log.Print("index executing error: ", err) 
-  	}
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Print("index parsing error: ", err)
+	}
+	err = t.Execute(w, MyPageVariables)
+	if err != nil {
+		log.Print("index executing error: ", err)
+	}
 }
